f972b20e12cd9a0ddc421faaa81ffee9f6cd7d37
emmaviolet <emmavioletmakinson@gmail.com>
2014-09-18 16:26:22 +0100

    first commit

---
+++ photo_sharing_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoSharingApp::Application.config.secret_token = 'b025d84f936e334574d57390b4588d061673115e71809c197452baf122e35e49c9a9ff91c1205d7ea4e1a6aee3ce9312e36dd492a6cd5d81ab0f24d070b381bc'
