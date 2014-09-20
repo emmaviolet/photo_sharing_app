cdd49992961ddde908f3f2ef63d020f73e884522
emmaviolet <emmavioletmakinson@gmail.com>
2014-09-20 12:22:12 +0100

    fixing users bug

--- photo_sharing_app/config/routes.rb
+++ photo_sharing_app/config/routes.rb
@@ -2,12 +2,12 @@ PhotoSharingApp::Application.routes.draw do
 
   resources :ratings
   resources :tags
-  resources :users
   resources :comments
   resources :albums
   devise_for :users
   resources :photos
 
+  get '/user_page/:id', to: 'users#show'
   root to: "photos#index"
   
 end
