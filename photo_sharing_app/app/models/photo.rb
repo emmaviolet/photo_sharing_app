f8063b24c8cdae35adaca67279f48f3f07c54349
Your Name <chakobre@gmail.com>
2014-09-20 16:00:29 +0100

    adding rmagick

--- photo_sharing_app/app/models/photo.rb
+++ photo_sharing_app/app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :album_id, :description, :image, :name, :photo_file
+  attr_accessible :album_id, :description, :image, :name, :photo_file, :remote_photo_file_url
   mount_uploader :photo_file, PhotoFileUploader
   has_many :ratings
   has_many :comments
--- photo_sharing_app/app/uploaders/photo_file_uploader.rb
+++ photo_sharing_app/app/uploaders/photo_file_uploader.rb
@@ -3,7 +3,7 @@
 class PhotoFileUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -32,9 +32,13 @@ class PhotoFileUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+   version :thumb do
+     process :resize_to_fill => [100, 100]
+   end
+
+   version :detail do
+      process :resize_to_fit => [400, 400]
+    end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
