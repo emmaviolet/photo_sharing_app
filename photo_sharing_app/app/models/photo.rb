d9421658838b15b1bb40b5d5d470249c4d57bc00
LaurenceRolandJames <laurencerolandjames@gmail.com>
2014-09-21 15:23:44 +0100

    adding add a comment form to photo show page

--- photo_sharing_app/app/models/photo.rb
+++ photo_sharing_app/app/models/photo.rb
@@ -1,9 +1,9 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :album_id, :description, :image, :name, :photo_file, :remote_photo_file_url
+  attr_accessible :album_id, :description, :image, :name, :photo_file, :remote_photo_file_url, :comments_attributes
   mount_uploader :photo_file, PhotoFileUploader
   has_many :ratings
   has_many :comments
   has_many :tags
   belongs_to :album
-
+  accepts_nested_attributes_for :comments
 end
