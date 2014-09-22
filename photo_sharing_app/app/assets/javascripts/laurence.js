fea3799ae0d40ec0f8373d860db95fad1ed67053
LaurenceRolandJames <laurencerolandjames@gmail.com>
2014-09-22 19:23:52 +0100

    adding starter premium site styling

---
+++ photo_sharing_app/app/assets/javascripts/laurence.js
@@ -0,0 +1,8 @@
+console.log('javascript works');
+
+// $(document).ready(function(){
+//   $('#new_comment').submit(function(){
+//     $.post($(this).attr("action"), $this.serialize(), null, "script");
+//     return false;
+//   });
+// });
