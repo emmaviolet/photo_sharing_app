ed2454e9526ed60ae9128a7f6e2a4870aaeb99ea
LaurenceRolandJames <laurencerolandjames@gmail.com>
2014-09-22 19:53:22 +0100

    commiting before push because the git demands it

--- photo_sharing_app/db/seeds.rb
+++ photo_sharing_app/db/seeds.rb
@@ -15,7 +15,7 @@ end
 album_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 40.times do |n|
-  a = Album.create(name: album_names.sample, user_id: User.all.sample.id)
+  a = Album.create name: "#{album_names.sample}", user_id: "#{User.all.sample.id}"
   a.save
 end
 
@@ -24,7 +24,7 @@ photo_names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday
 photo_images = ["http://3.bp.blogspot.com/_rvHbl_Xm8kk/TURQ_LiMzgI/AAAAAAAABjA/zTS6t0LJYds/s1600/eiffel%2Btower.jpg", "http://i.telegraph.co.uk/multimedia/archive/02524/maldives_2524920b.jpg", "http://kyoto-brand.com/column-img/101207/101207_2005_11kiyomizudera.jpg"]
 
 100.times do |n|
-  p = Photo.create(name: photo_names.sample, album_id: Album.all.sample.id, remote_photo_file_url: photo_images.sample)
+  p = Photo.create name: "#{photo_names.sample}", album_id: "#{Album.all.sample.id}", remote_photo_file_url: "#{photo_images.sample}"
   p.save
 end
 
@@ -38,25 +38,25 @@ end
 rating_values = [1, -1]
 
 300.times do |n|
-  r = Rating.create value: rating_values.sample, photo_id: Photo.all.sample.id, user_id: User.all.sample.id
+  r = Rating.create value: "#{rating_values.sample}", photo_id: "#{Photo.all.sample.id}", user_id: "#{User.all.sample.id}"
   r.save
 end
 
 comment_texts = ["awesome!", "great!", "loved it", "a bit rubbish", "something else please", "not as good as my photo"]
 
 300.times do |n|
-  c = Comment.create text: comment_texts.sample, photo_id: Photo.all.sample.id, user_id: User.all.sample.id
+  c = Comment.create text: "#{comment_texts.sample}", photo_id: "#{Photo.all.sample.id}", user_id: "#{User.all.sample.id}"
   c.save
 end
 
 tag_texts = ["funny", "cats", "dogs", "lazy", "sunny", "yolo"]
 
 300.times do |n|
-  t = Tag.create text: tag_texts.sample, photo_id: Photo.all.sample.id
+  t = Tag.create text: "#{tag_texts.sample}", photo_id: "#{Photo.all.sample.id}"
   t.save
 end
 
 # default photo
 
-p = Photo.create name: "default_album_cover_image", image: "http://cdn3.itzgeek.com/wp-content/themes/daily/images/default-thumb.gif"
-p.save
+p = Photo.create name: "default_album_cover_image", remote_photo_file_url: "http://cdn3.itzgeek.com/wp-content/themes/daily/images/default-thumb.gif"
+p.save(:validate => false)
