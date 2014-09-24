ca024a80a9303b0e0225fd4ba155c2a44490f72a
LaurenceRolandJames <laurencerolandjames@gmail.com>
2014-09-24 15:21:26 +0100

    adding logic to comments forms so they can be hidden or revealed accordingly

--- photo_sharing_app/app/assets/javascripts/laurence.js
+++ photo_sharing_app/app/assets/javascripts/laurence.js
@@ -1,17 +1,30 @@
 console.log('javascript works');
 
+// $('#addCommentButton').click(function(e) {
+//   e.preventDefault();
+
+
 $(document).ready(function() {
 
-  $('#addCommentButton').click(function(e) {
-    e.preventDefault();
-    $('#addComment').show('slow', function() {
-    });
+  $('#addCommentButton').click(function() {
+    if ($('#addComment')[0].style.display === 'none'){
+      $('#addComment').show('slow', function() {
+      });
+    } else {
+      $('#addComment').hide('slow', function() {
+      });
+    }
   });
 
-  $('.editCommentButton').click(function(e) {
-    e.preventDefault();
-    $(this).next('.editComment').show('slow', function() {
-    });
+  $('.editCommentButton').click(function() {
+    if ($(this).next('.editComment')[0].style.display === 'none'){
+      $(this).next('.editComment').show('slow', function() {
+      });
+    } else {
+      $(this).next('.editComment').hide('slow', function() {
+      });
+    }
   });
 
 });
+
