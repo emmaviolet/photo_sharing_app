366f962f55d383475185f7a77bec84eef920eb22
LaurenceRolandJames <laurencerolandjames@gmail.com>
2014-09-24 13:20:37 +0100

    adding appearing edit comments boxes with jquery booooooooooom

--- photo_sharing_app/app/assets/javascripts/laurence.js
+++ photo_sharing_app/app/assets/javascripts/laurence.js
@@ -1,9 +1,17 @@
 console.log('javascript works');
 
 $(document).ready(function() {
-  
+
   $('#addCommentButton').click(function(e) {
     e.preventDefault();
     $('#addComment').show('slow', function() {
     });
   });
+
+  $('.editCommentButton').click(function(e) {
+    e.preventDefault();
+    $(this).next('.editComment').show('slow', function() {
+    });
+  });
+
+});
