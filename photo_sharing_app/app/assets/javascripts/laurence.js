9b01f6741651867379cb2227e13c03f44cd1d285
LaurenceRolandJames <laurencerolandjames@gmail.com>
2014-09-24 12:18:52 +0100

    ecstatically, adding an add comment form that appears using javascript wooooooooo

--- photo_sharing_app/app/assets/javascripts/laurence.js
+++ photo_sharing_app/app/assets/javascripts/laurence.js
@@ -1,8 +1,5 @@
 console.log('javascript works');
 
-// $(document).ready(function(){
-//   $('#new_comment').submit(function(){
-//     $.post($(this).attr("action"), $this.serialize(), null, "script");
-//     return false;
-//   });
-// });
+function addComment() {
+   document.getElementById('addComment').style.display = "block";
+}
